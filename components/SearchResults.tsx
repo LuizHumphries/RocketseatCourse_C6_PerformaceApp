755a25a848583f0232ce9462fcd0a070bce32865
Luiz Humphries <luizgustavo.frota@outlook.com>
2022-12-19 21:13:29 -0300

    virtualized

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from "react-virtualized";
 import { ProductItem } from "./ProductItem";
 
 type SearchResultsProps = {
@@ -16,14 +17,24 @@ export function SearchResults({
   results,
   addToWishList,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem product={results[index]} addToWishList={addToWishList} />
+      </div>
+    );
+  };
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => {
-        return (
-          <ProductItem key={product.id} product={product} addToWishList={addToWishList} />
-        );
-      })}
+      <List
+        height={390}
+        rowHeight={30}
+        width={900}
+        overscanColumnCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
